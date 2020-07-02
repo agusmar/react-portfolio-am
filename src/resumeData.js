95d8d956757b6ece66cbb8545621ae769b1e4f10
Agus Mar <agustinam@gmail.com>
2020-07-02 13:14:29 -0300

    inital commit

---
+++ src/resumeData.js
@@ -0,0 +1,63 @@
+let resumeData = {
+    "imagebaseurl":"https://https://media-exp1.licdn.com/dms/image/C4E03AQF5N7Y2vSpKRw/profile-displayphoto-shrink_200_200/0?e=1598486400&v=beta&t=OY2vXP4Fizjb04-2gw3G3LOWFaQdLe6of4WmXwQY7VM.github.io/",
+    "name": "Agustina Marijanovic",
+    "role": "Frontend Developer",
+    "linkedinId":"Your LinkedIn Id",
+    "roleDescription": " I´m very curious, restless, and eager to learn about new technologies and methodologies.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/agustinamar/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/agusmar",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://twitter.com/straycatdev",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"Having recently concluded an intensive course in Frontend Development, I have been working on independent projects in order to fine-tune my understanding as well as learn new tools, methods, libraries, frameworks, and programming languages.",
+    "address":"Buenos Aires, Argentina",
+    "website":"www.agustina.com",
+       "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+  }
+  
+  export default resumeData
